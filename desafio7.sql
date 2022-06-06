829b591bee4b90e651d4ecc14229ef97bc8539a1
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-06-06 10:52:05 -0300

    requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,11 @@
 SELECT
-    album.name_album AS album,
+album.name_album AS album,
     artist.name_artist AS artista,
     COUNT(followers.artist_id) AS seguidores
+FROM SpotifyClone.album AS album
+RIGHT JOIN SpotifyClone.artist AS artist
+ON album.artist_id =artist.artist_id
+INNER JOIN SpotifyClone.follow_artist as followers
+ON followers.artist_id = album.artist_id
+GROUP BY artista, album
+ORDER BY seguidores DESC, artista, album;
